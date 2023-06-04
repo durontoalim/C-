9b6a07deb43f81028e9a27f0794d58b24e860e4f
Md Abdul Alim <abdulalimdhuronto24@gmail.com>
2023-06-04 10:29:25 +0600

    update

--- main.cpp
+++ main.cpp
@@ -1,81 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[], int l, int m, int r)
+int32_t main()
 {
-	int i, j, k;
-	int n1 = m - l + 1;
-	int n2 = r - m;
+    int arr[] = {21,20,32,27,45,29};
 
-	/* create temp arrays */
-	int L[n1], R[n2];
-
-	/* Copy data to temp arrays L[] and R[] */
-	for (i = 0; i < n1; i++)
-		L[i] = arr[l + i];
-	for (j = 0; j < n2; j++)
-		R[j] = arr[m + 1 + j];
-
-	/* Merge the temp arrays back into arr[l..r]*/
-	i = 0; // Initial index of first subarray
-	j = 0; // Initial index of second subarray
-	k = l; // Initial index of merged subarray
-	while (i < n1 && j < n2) {
-		if (L[i] <= R[j]) {
-			arr[k] = L[i];
-			i++;
-		}
-		else {
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-
-	/* Copy the remaining elements of L[], if there
-	are any */
-	while (i < n1) {
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	/* Copy the remaining elements of R[], if there
-	are any */
-	while (j < n2) {
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
-}
-
-/* l is for left index and r is right index of the
-sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
-{
-	if (l < r) {
-		// Same as (l+r)/2, but avoids overflow for
-		// large l and h
-		int m = l + (r - l) / 2;
-
-		// Sort first and second halves
-		mergeSort(arr, l, m);
-		mergeSort(arr, m + 1, r);
-
-		merge(arr, l, m, r);
-	}
-}
-
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int arr_size = sizeof(arr) / sizeof(arr[0]);
-
-	mergeSort(arr, 0, arr_size - 1);
-
-	for (int i = 0; i < arr_size; i++)
+    int len = sizeof(arr)/sizeof(arr[0]);
+    // Sort
+    for (int i = 0; i < len; i++)
     {
-        cout<<arr[i]<<" ";
+        for (int j = i+1; j < len; j++)
+        {
+            if(arr[i] > arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
-	return 0;
+    printf("Sort List: ");
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    // sum all element
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += arr[i];
+    }
+    printf("Sum: %d\n",sum);
+    // sum all digit sumation
+    int sum2 = 0;
+
+    int sub = sum;
+    while (sub > 0)
+    {
+        int mod = sub%10;
+        sum2 += mod;
+
+        sub /= 10;
+    }
+
+    printf("Digit Sum: %d\n",sum2);
+    
+    
+    
+    
+
+    return 0;
 }
